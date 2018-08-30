f3f805aed453f10c8aa8c50de0e986d931e4200a
Shiah <backcountryresearch@gmail.com>
2018-08-29 17:08:39 -0700

    added a function so that the main app is returened venues in a radius insted of just the nearest 100 venues

--- find_nearest_venues.py
+++ find_nearest_venues.py
@@ -1,17 +1,19 @@
 
 import pandas as pd
 import numpy as np
+import sys
+sys.path.insert(0, '/Users/shiahlints/galvanize/FIXGITPROBLEM/booking-agent-ai/data')
 #from math import radians, cos, sin, asin, sqrt
 
 
-#venues = pd.read_csv('venues_with_clean_bios.csv')
+venues = pd.read_pickle('data/master_venues_df.p')
+cities = pd.read_csv('data/uscitiesv1.4.csv')
 
-#cities = pd.read_csv('uscitiesv1.4.csv')
 class Venue_Finder(object):
     """this class finds the nearest venues to a point. Either another
     venue or a city state"""
-    def __init__(self, venues, citys_filepath = '../data/uscitiesv1.4.csv'):
-        self.cities = pd.read_csv(citys_filepath)
+    def __init__(self, venues = venues, cities= cities):
+        self.cities = cities
         self.venues = venues
 
 
@@ -19,10 +21,12 @@ class Venue_Finder(object):
     def get_nearest_venues_by_city(self, city, state, milage=10):
         lat, lng = self.cities[(self.cities['city'] == city) & \
         (self.cities['state_id']== state)][['lat','lng']].values[0]
-        venues = self.find_at_least_100(lat, lng)
+        venues = self.find_venues_by_milage(lat, lng, milage)
         return venues
 
+
     def get_nearest_venues_by_venue(self, venue_id, n):
+        '''used for testing'''
         if venue_id not in self.venues.venue_id.values:
             print ('This is a new venue')
         venue = self.venues[self.venues['venue_id']==venue_id]
@@ -34,7 +38,14 @@ class Venue_Finder(object):
             nearest_venues = pd.concat([nearest_venues, thing_to_add], axis = 0)
         return nearest_venues
 
-    def find_at_least_n(self, lat, lng, n):
+    def find_venues_by_milage(self, lat, lng, milage = 10):
+        array = self.venues[['latitude', 'longitude']].values
+        miles_array = self.haversine_np(lat, lng, array)
+        #return miles_array
+        milage_filter = miles_array < milage
+        return self.venues.iloc[milage_filter]
+
+    def find_at_least_n_venues(self, lat, lng, n):
         array = self.venues[['latitude', 'longitude']].values
         miles_array = self.haversine_np(lat, lng, array)
         hundred_nearest = np.argsort(miles_array)[0:n]
