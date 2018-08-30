6f41cc015359a317087ab52be1be98f751e9a5a7
Shiah <backcountryresearch@gmail.com>
2018-08-29 17:01:16 -0700

    This is the app the will be run from the cammand line or a web interface

---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import cosinrecomender
+
+#master_venues = pd.read_pickle('data/master_venues_df.p')
+master_artists = pd.read_pickle('data/master_artists_df.p')
+citys = pd.read_csv('data/uscitiesv1.4.csv').city.values
+#top_n = number_of_recomendations
+recomender = cosinrecomender.Recomender()
+def recomend():
+    #user_type = input('''Are you a fan looking for a show \n or a artist looking for a venue.\nPlease enter F or A''')
+    state_id = input('''What State do you want to search in?WA
+Please enter WA, CA or OR  ''')
+    city = input('''What city do you want to center you search on'''+
+    ''' Please make the first word a cap''')
+    radius = input('''What is the radius in miles you would like to search?''')
+    radius = int(radius)
+    artist_name = input('Please enter the name of your band')
+    artist = (master_artists[master_artists.artist_name == artist_name])
+    artist_bio = artist.bio.values[0]
+    artist_genre = artist.genre.values[0]
+    result = recomender.get_best_fit_for_artist(city, state_id, radius,\
+                                artist_genre = artist_genre, artist_bio = artist_bio)
+    return result
