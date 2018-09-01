84414eb4637495dd80e1e26baceebd52ffe095ed
Shiah <backcountryresearch@gmail.com>
2018-09-01 16:04:35 -0700

    The web app

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ citys = pd.read_csv('data/uscitiesv1.4.csv').city.values
 recomender = cosinrecomender.Recomender()
 def recomend():
     #user_type = input('''Are you a fan looking for a show \n or a artist looking for a venue.\nPlease enter F or A''')
-    state_id = input('''What State do you want to search in?WA
+    state_id = input('''What State do you want to search in?
 Please enter WA, CA or OR  ''')
     city = input('''What city do you want to center you search on'''+
-    ''' Please make the first word a cap''')
-    radius = input('''What is the radius in miles you would like to search?''')
+    ''' Please make the first word a cap ''')
+    radius = input('''What is the radius in miles you would like to search? ''')
     radius = int(radius)
-    artist_name = input('Please enter the name of your band')
+    artist_name = input('Please enter the name of your band ')
     artist = (master_artists[master_artists.artist_name == artist_name])
-    artist_bio = artist.bio.values[0]
-    artist_genre = artist.genre.values[0]
+    artist_bio = artist.artist_bio.values[0]
+    artist_genre = artist.artist_genre.values[0]
     result = recomender.get_best_fit_for_artist(city, state_id, radius,\
                                 artist_genre = artist_genre, artist_bio = artist_bio)
     return result
